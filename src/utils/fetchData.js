9e103dc29ce2e144a8b06c5c29a4a47c4bf32b6b
SubhamsSharma <61783465+SubhamsSharma@users.noreply.github.com>
2023-02-09 10:38:52 +0530

    Initial commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+import REACT_APP_RAPID_API_KEY from "../../.env";
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+  }
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com"
+  }
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = res.json();
+
+  return data;
+};
